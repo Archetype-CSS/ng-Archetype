29bd1eb746679cfcf0daf1c860ac390656b2b4c9
Sam Ashley <sashley@providigm.com>
2015-01-25 16:25:22 -0700

    mist tweaks, style fixes for ocn

---
+++ app/scripts/controllers/offCanvasNavCtrl.js
@@ -0,0 +1,15 @@
+
+
+/* do this in native js */
+
+/*
+$(window).resize(function() {     // convert this to native js 
+  //update stuff one window resize event
+});
+
+*/
+
+/* get the height of the window */
+//var viewPortHeight = $(window).height();
+/* set the offCanvas Nav's height to the height of the window */
+
